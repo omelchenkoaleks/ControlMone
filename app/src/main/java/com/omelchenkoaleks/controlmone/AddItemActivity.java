9c2122c225f3003b574fd0071317aeb8d98c2d20
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-11-11 14:56:56 +0200

    Добавил в проект icon.png.

---
+++ app/src/main/java/com/omelchenkoaleks/controlmone/AddItemActivity.java
@@ -0,0 +1,13 @@
+package com.omelchenkoaleks.controlmone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
