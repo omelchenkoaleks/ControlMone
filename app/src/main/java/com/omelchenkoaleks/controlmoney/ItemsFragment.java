e6a007107d244cb3b44b407aa96208f6e8c73dd4
omelchenkoaleks <omelchenkoaleks@gmail.com>
2018-11-14 12:10:28 +0200

    Пример использования AsyncTask для получения данных по API.

--- app/src/main/java/com/omelchenkoaleks/controlmoney/ItemsFragment.java
+++ app/src/main/java/com/omelchenkoaleks/controlmoney/ItemsFragment.java
@@ -1,8 +1,8 @@
 package com.omelchenkoaleks.controlmoney;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -78,53 +78,99 @@ public class ItemsFragment extends Fragment {
         loadItems();
     }
 
+    @SuppressLint("StaticFieldLeak")
     private void loadItems() {
 
-        Log.d(TAG, "loadItems: current thread " + Thread.currentThread().getName());
+        AsyncTask<Void, Void, List<Item>> task = new AsyncTask<Void, Void, List<Item>>() {
 
-        new LoadItemsTask(new Handler(callback)).start();
+            // Метод выполнится в главном потоке. В нем можно что-то сделать до выполнения
+            // нашей основной задачи - например показать прогресс бар до загрузки.
+            @Override
+            protected void onPreExecute() {
+                Log.d(TAG, "onPreExecute: thread name " + Thread.currentThread().getName());
+            }
+
+            // Метод будет выполнен в другом потоке, который будет создан для непосредственной
+            // нашей задачи...
+            @Override
+            protected List<Item> doInBackground(Void... voids) {
+                Log.d(TAG, "doInBackground: thread name " + Thread.currentThread().getName());
+
+                Call<List<Item>> call = api.getItems(type);
+                try {
+                    List<Item> items = call.execute().body();
+                    // Если пришли то возвращаем...
+                    return items;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // Если не пришли...
+                return null;
+            }
+
+            // В этом методе данные вернутся в главный поток.
+            @Override
+            protected void onPostExecute(List<Item> items) {
+                // Но, конечно, нужно проверить, что они не равны null.
+                if (items != null) {
+                    adapter.setData(items);
+                }
+            }
+        };
+
+        // После создания его нужно запустить.
+        task.execute();
     }
 
-    private Handler.Callback callback = new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
 
-            if (msg.what == DATA_LOADED) {
-                List<Item> items = (List<Item>) msg.obj;
-                adapter.setData(items);
-            }
-            return true;
-        }
-    };
-
-    private final static int DATA_LOADED = 123;
-
-    private class LoadItemsTask implements Runnable {
-
-        private Thread thread;
-        private Handler handler;
-
-        public LoadItemsTask(Handler handler) {
-            thread = new Thread(this);
-            this.handler = handler;
-        }
-
-        public void start() {
-            thread.start();
-        }
-
-        @Override
-        public void run() {
-
-            Log.d(TAG, "run: current thread " + Thread.currentThread().getName());
-
-            Call<List<Item>> call = api.getItems(type);
-            try {
-                List<Item> items = call.execute().body();
-                handler.obtainMessage(DATA_LOADED, items).sendToTarget();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    // Thread and Handler. 1 Версия получения данных из сети.
+//    private void loadItems() {
+//
+//        Log.d(TAG, "loadItems: current thread " + Thread.currentThread().getName());
+//
+//        new LoadItemsTask(new Handler(callback)).start();
+//    }
+//
+//    private Handler.Callback callback = new Handler.Callback() {
+//        @Override
+//        public boolean handleMessage(Message msg) {
+//
+//            if (msg.what == DATA_LOADED) {
+//                List<Item> items = (List<Item>) msg.obj;
+//                adapter.setData(items);
+//            }
+//            return true;
+//        }
+//    };
+//
+//    private final static int DATA_LOADED = 123;
+//
+//    private class LoadItemsTask implements Runnable {
+//
+//        private Thread thread;
+//        private Handler handler;
+//
+//        public LoadItemsTask(Handler handler) {
+//            thread = new Thread(this);
+//            this.handler = handler;
+//        }
+//
+//        public void start() {
+//            thread.start();
+//        }
+//
+//        @Override
+//        public void run() {
+//
+//            Log.d(TAG, "run: current thread " + Thread.currentThread().getName());
+//
+//            Call<List<Item>> call = api.getItems(type);
+//            try {
+//                List<Item> items = call.execute().body();
+//                handler.obtainMessage(DATA_LOADED, items).sendToTarget();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
