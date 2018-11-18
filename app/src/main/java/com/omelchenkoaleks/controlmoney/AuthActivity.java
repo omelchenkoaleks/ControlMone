f9eaf8f63fb78d3f91e183713dc6b428eb5218d5
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-11-18 06:24:58 +0200

    Реализовал проверку на успех и неуспех при получении аккаунта. И само подключение к аккаунту Гугл.

--- app/src/main/java/com/omelchenkoaleks/controlmoney/AuthActivity.java
+++ app/src/main/java/com/omelchenkoaleks/controlmoney/AuthActivity.java
@@ -1,15 +1,20 @@
 package com.omelchenkoaleks.controlmoney;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -53,5 +58,47 @@ public class AuthActivity extends AppCompatActivity {
 
         Intent signInIntent = googleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    // Метод вызывается, когда мы получили аккаунт.
+    private void updateUI(GoogleSignInAccount account) {
+        if (account == null) {
+            showError();
+        } else {
+            showSuccess();
+        }
+    }
+
+    private void showSuccess() {
+        Toast.makeText(this, "Account successfully obtained", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showError() {
+        Toast.makeText(this, "Account is null", Toast.LENGTH_SHORT).show();
     }
 }
